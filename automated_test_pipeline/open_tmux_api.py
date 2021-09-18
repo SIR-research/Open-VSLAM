5375cc6c99067e6aa803b598405ea26fe9aedc4b
PauloRodriguesJr <pauloe_jr@hotmail.com>
2021-09-17 23:34:41 -0300

    Update layout, add bag recording and stdout logs saving - "Reduced" final version

--- automated_test_pipeline/open_tmux_api.py
+++ automated_test_pipeline/open_tmux_api.py
@@ -9,7 +9,7 @@ import time
 from typing import List
 
 # TODO: replace by arg-parse
-debugging = True
+debugging = False
 slam = True
 
 # ***** Choose between the slam and localization task *****
@@ -18,24 +18,27 @@ if slam:
 else:
     task = 'localization'
 
-base_path = '/home/paulo/Projects/openvslam_ws/openvslam/build/'  # openvslam_path
+base_path = '/home/paulo/Projects/openvslam_ws/openvslam/build/'
 tests_ws_path = '/home/paulo/Projects/IC_SLAM_pipelines'
 
 if debugging:
     test_type = 'experiment_data_debug'
     frameskip_list = [0]  # [0, 3]
-    resolution_list = [730]  # [720, 1080]
+    resolution_list = [720]  # [720, 1080]
 else:
-    frameskip_list = [0, 1, 2, 3, 4, 5, 6]  # The objetive experiment
+    frameskip_list = [0]  # [0, 1, 2, 3, 4, 5, 6]  # The objetive experiment
     test_type = 'experiment_data'
-    resolution_list = [320, 480, 720, 1080]
+    resolution_list = [720]  # [320, 480, 720, 1080]
 
 
 video_list = glob.glob(tests_ws_path+'/{}/videos/*.MP4'.format(test_type))
 mask_list = glob.glob(tests_ws_path+'/{}/masks/*.png'.format(test_type))
 config_path = tests_ws_path+'/{}/videos/gopro.yaml'.format(test_type)
-vocab_path = '/home/paulo/Projects/openvslam_ws/openvslam/orb_vocab/orb_vocab.dbow2'
 map_path = tests_ws_path+'/{}/maps/'.format(test_type)
+bag_path = tests_ws_path+'/{}/bags/'.format(test_type)
+log_path = tests_ws_path+'/{}/logs/'.format(test_type)
+vocab_path = '/home/paulo/Projects/openvslam_ws/openvslam/orb_vocab/orb_vocab.dbow2'
+
 
 print(video_list)
 print(mask_list)
@@ -43,7 +46,7 @@ print(frameskip_list)
 time.sleep(1)
 
 
-for i, video_path in enumerate(video_list):
+for i, video_path in enumerate(video_list[:2]):
     # Name structure encoding: name-FPS-RESOLUTION-MASK
 
     resolution = video_path.split('-')[2]
@@ -56,10 +59,10 @@ for i, video_path in enumerate(video_list):
         if(mask_name.startswith('mask{}'.format(resolution))):
             mask_valid.append(mask)
     # print(mask_valid)
-    time.sleep(2)
+    time.sleep(1)
     # Match the videos wich has the same resolution of the mask
 
-    for mask in mask_valid:
+    for mask in mask_valid[:1]:
         for frameskip in frameskip_list:
             fps = int(120/(1+frameskip))
 
@@ -70,10 +73,12 @@ for i, video_path in enumerate(video_list):
             map_name_suffix = (mask.split(".")[0]).split("-")[-1]
             map_name_args.append(map_name_suffix)
             map_name = f"map-{'-'.join(map_name_args)}.msg"
-            db_name = f"db-{'-'.join(map_name_args)}.pkl"
+            # --- replace by a rosbag hehe
+            bag_name = f"bag-{'-'.join(map_name_args)}-{task}.bag"
+            log_name = f"log-{'-'.join(map_name_args)}-{task}.txt"
 
             print(map_name)
-            print(db_name)
+            print(bag_name)
 
             # # the tmux part
             windows = []
@@ -86,12 +91,14 @@ for i, video_path in enumerate(video_list):
             win.cmd('split-window', '-h')
             win.cmd('split-window', '-f')
             win.cmd('split-window', '-h')
+            win.cmd('split-window', '-h')
+            win.cmd('select-layout', 'tiled')
 
             panes.extend(win.list_panes())
             command_list = [
                 "roscore",
                 "rosrun image_transport republish raw in:=/video/image_raw raw out:=/camera/image_raw",
-                f"rosrun openvslam run_slam  \
+                f"stdbuf -oL rosrun openvslam run_{task}  \
                 -v {vocab_path}  \
                 -c {config_path} \
                 --frame-skip {frameskip} \
@@ -100,8 +107,10 @@ for i, video_path in enumerate(video_list):
                 --eval-log \
                 --mask {mask} \
                 --map-db {map_path+map_name} \
-                && tmux kill-session -t openvslam_tests",
-                f"rosrun publisher video -m {video_path} && tmux send-keys -t 2 C-c"
+                &> {log_path+log_name} && \
+                tmux kill-session -t openvslam_tests",
+                f"rosrun publisher video -m {video_path} &&  tmux send-keys -t 4 C-c  && tmux send-keys -t 2 C-c && sleep 2",
+                f"rosbag record -O {bag_path+bag_name} /clock /openvslam/camera_pose /openvslam/odometry /tf"
             ]
 
             print(panes)
